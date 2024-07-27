3a6874b09eb51b1051f77030606a7b37f750d4ea
sedusik <sedusova.tanya@yandex.ru>
2024-07-28 01:32:37 +0300

    Get a list of people(CRUD for person)

--- resources/js/components/Person/Index.vue
+++ resources/js/components/Person/Index.vue
@@ -1,12 +1,48 @@
 <template>
     <div>
-        Index
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Job</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="person in people">
+                <td>{{ person.name}}</td>
+                <td>{{ person.age}}</td>
+                <td>{{ person.job}}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    name: "Index"
+    name: "Index",
+
+    data() {
+        return {
+            people: null
+        }
+    },
+
+    mounted() {
+        this.getPeople();
+    },
+
+    methods: {
+        getPeople() {
+            axios.get('/api/people')
+                .then( res => {
+                    this.people = res.data
+                })
+        }
+    }
 }
 </script>
 
